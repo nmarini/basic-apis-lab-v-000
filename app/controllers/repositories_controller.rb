9003915ddea6f0193996d372ec26084bddc83252
Nicolas Marini <nmarini44@gmail.com>
2019-06-04 23:59:37 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -10,7 +10,7 @@ class RepositoriesController < ApplicationController
       req.params['client_secret'] =
       req.params['q'] = params[:query]
     end
-    @body = JSON.parse(@resp.body)["repository"]
+    @body = JSON.parse(@resp.body)["items"]["repositories"]
 
     # if @resp.success?
     #   @repositories = body["response"]
